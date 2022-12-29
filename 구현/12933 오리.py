b7895b0b3b67561c8e8b8b6429eca5197693d8b9
yuna <yuna4490@naver.com>
2022-12-29 23:33:56 +0900

    백준 실버3 (F -> P)

--- 구현/12933 오리.py
+++ 구현/12933 오리.py
@@ -1,33 +1,35 @@
-sound = input()
-quack = "quack"
-visited = [0] * len(sound)
-cnt = 0
+quack = 'quack'
+duck = input()
+visited = [0] * len(duck)
 
 def find(start):
     global cnt
-    j = 1
-    first = True
-    for i in range(start+1, len(sound)):
-        if sound[i] == quack[j] and not visited[i]:
+    j = 0
+    first = 1
+    for i in range(start, len(duck)):
+        if duck[i] == quack[j] and not visited[i]: # 울음소리 비교
             visited[i] = 1
-            if sound[i] == 'k': # 마지막 문자
-                if first: # 만약 처음 울음소리가 완성된 거라면
-                    first = False
-                    cnt += 1
-                j = 1
+            if duck[i] == 'k':
+                if first:
+                    cnt += 1 # 오리가 연속으로 울면 한 마리로 치기 때문에 처음만 count 해줌
+                    first = 0 # 똑같은 오리가 울 경우에 다음부턴 count 안해주기 위해서
+                j = 0 # 마지막 문자인 k까지 나왔으면 다시 q부터 찾아서 비교해야 함
             
             else:
-                j += 1
+                j += 1 # k가 아니면 순서대로 다음 문자를 비교하기 위해 index ++
 
-if len(sound) % 5 != 0: # 5의 배수가 아니라면 올바르지 않은 소리
+
+if len(duck) % 5 != 0: # 입력된 문자열의 길이가 5의 배수가 아니라면 올바르지 않은 울음소리임
     print(-1)
+    exit() # -1을 출력하고 exit를 해주어야 프로그램이 종료되어 다음 for문 실행이 안됨
 
-for i in range(len(sound)):
-    if sound[i] == 'q' and not visited[i]: # 방문하지 않은 q가 있으면
+cnt = 0 # 오리의 수
+for i in range(len(duck)):
+    if duck[i] == 'q' and not visited[i]:
         find(i)
 
-if cnt == 0 or not all(visited): # 오리가 0마리이거나 모든 문자를 방문하지 않았을 경우
-    print(-1) # 올바르지 않은 소리
+if cnt == 0 or not all(visited): # 오리의 수가 0마리거나 모든 문자를 방문하지 않았을 경우
+    print(-1) # 올바르지 않은 울음소리
 
-else:
+else: # 올바른 울음소리일 경우 오리의 수 출력
     print(cnt)
