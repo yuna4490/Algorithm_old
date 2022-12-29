4786157c3a3bd376906b297e1b854fc13313fbd1
yuna <yuna4490@naver.com>
2022-12-29 19:49:22 +0900

    백준 실버3 (F)

--- 브루트포스/2503 숫자 야구.py
+++ 브루트포스/2503 숫자 야구.py
@@ -1,20 +1,21 @@
 from itertools import permutations
 n = int(input())
 
-nums = permutations(range(1, 10), 3)
+nums = list(permutations(range(1, 10), 3))
 
 
 for _ in range(n):
     x, s, b = map(int, input().split())
+    x = str(x)
     rmcnt = 0
     for i in range(len(nums)):
         strike = ball = 0
         i -= rmcnt # 매번 nums[0]부터 시작하기 위해
         for j in range(3):
-            if nums[i][j] == x[j]:
+            if str(nums[i][j]) == x[j]:
                 strike += 1
             
-            elif str(x[j]) in nums[i]:
+            elif str(x[j]) in str(nums[i]):
                 ball += 1
         
         if (strike != s) or (ball != b):
