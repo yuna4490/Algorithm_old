a302a923de6719560cf0eae0181b04b6bd6476c9
yuna <yuna4490@naver.com>
2023-01-04 16:49:45 +0900

    소프티어 lv.2 (P)

---
+++ Softeer/장애물 인식 프로그램.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+result = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(i, j):
+    queue = deque()
+    queue.append((i, j))
+    graph[i][j] = 0
+    cnt = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+                cnt += 1
+    return cnt
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            result.append(bfs(i, j))
+
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
