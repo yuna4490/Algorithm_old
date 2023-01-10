3050c6f4c5ce66d1a44013ec6bb6b1f360d2b785
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-01-10 20:14:06 +0900

    백준 브론즈1 (F) 메모리 초과..

---
+++ 정렬/10989 수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+import time
+input = sys.stdin.readline
+
+n = int(input())
+nums = [0] * 10001
+
+for _ in range(n):
+    nums[int(input())] += 1
+
+for i in range(1, 10001):
+    if nums[i] != 0:
+        for j in range(nums[i]):
+            print(i)
