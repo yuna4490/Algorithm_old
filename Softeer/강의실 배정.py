68ae308ccf402586c524253dbe9712d47dd77fee
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-01-17 02:40:24 +0900

    소프티어 Lv.3 (P)

--- Softeer/강의실 배정.py
+++ Softeer/강의실 배정.py
@@ -6,14 +6,14 @@ heap = []
 n = int(input())
 for _ in range(n):
     s, f = map(int, input().split())
-    heapq.heappush(heap, (f, s)) # 끝나는 시간 순으로 우선순위 정렬됨
+    heapq.heappush(heap, (f, s))
 
-x = 0
+tmp = 0
 cnt = 0
 while heap:
     f, s = heapq.heappop(heap)
-    if s >= x: # 시작시간이 이전의 끝나는 시간보다 크거나 같다면
-        x = f
+    if s >= tmp: #시작시간이 이전의 끝나는 시간보다 크거나 같다면
+        tmp = f
         cnt += 1
 
 print(cnt)
