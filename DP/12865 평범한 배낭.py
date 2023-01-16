3b9d9aadc8d8821157a0335fc639e1b6a5b962df
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-01-17 02:49:00 +0900

    백준 골드5 (P)

--- DP/12865 평범한 배낭.py
+++ DP/12865 평범한 배낭.py
@@ -1,6 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
-items = [(0, 0)]
 dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+items = [0]
+
 for _ in range(n):
     w, v = map(int, input().split())
     items.append((w, v))
@@ -9,10 +13,11 @@ for i in range(1, n+1):
     w = items[i][0]
     v = items[i][1]
     for j in range(1, k+1):
-        if j < w:
+        if w > j:
             dp[i][j] = dp[i-1][j]
         
         else:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
 
 print(dp[n][k])
+
