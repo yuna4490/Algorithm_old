b733c848025c334f95168251302c2f16972882dc
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-01-20 19:58:12 +0900

    백준 실버2 (P)

--- 이분탐색/2805 나무 자르기.py
+++ 이분탐색/2805 나무 자르기.py
@@ -4,15 +4,14 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 trees = list(map(int, input().split()))
 
-low, high = 1, max(trees)
+low, high = 1, max(trees) - 1
 
 while low <= high:
-    sum = 0
     mid = (low + high) // 2
-    
+    sum = 0
     for tree in trees:
         if tree > mid:
-            sum += (tree - mid)
+            sum += tree - mid
 
     if sum >= m:
         low = mid + 1
