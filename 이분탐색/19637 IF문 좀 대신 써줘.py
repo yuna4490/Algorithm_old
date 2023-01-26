2ed378fb05ab022a3a638cbc82f99383da5afa3e
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-01-26 20:28:37 +0900

    백준 실버3 (P)

--- 이분탐색/19637 IF문 좀 대신 써줘.py
+++ 이분탐색/19637 IF문 좀 대신 써줘.py
@@ -4,22 +4,18 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 rank = [input().split() for _ in range(n)]
 
-def bs(x):
-    low, high = 0, len(rank)-1
+for _ in range(m):
+    power = int(input())
+    low, high = 0, n-1
     result = 0
 
     while low <= high:
         mid = (low + high) // 2
-
-        if int(rank[mid][1]) >= x:
+        if int(rank[mid][1]) >= power:
             high = mid - 1
             result = mid
         
         else:
             low = mid + 1
     
-    return result
-
-for _ in range(m):
-    power = int(input())
-    print(rank[bs(power)][0])
+    print(rank[result][0])
