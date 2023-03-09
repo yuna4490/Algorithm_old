7a97938c1802caed27f048f6bc8d354f98d1870c
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-03-09 20:29:20 +0900

    P

---
+++ 프로그래머스/Lv.2/멀리 뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [0] * (n+2)
+    
+    dp[1] = 1
+    dp[2] = 2
+
+    if n > 2:    
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+            
+    return dp[n] % 1234567
