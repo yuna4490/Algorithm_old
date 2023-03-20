a069ec896d22a356b09050e1d8362241ad6153b2
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-03-20 20:00:00 +0900

    백준 실버1 (P)

--- DFS/BFS/1389 케빈 베이컨의 6단계 법칙 다시.py
+++ DFS/BFS/1389 케빈 베이컨의 6단계 법칙 다시.py
@@ -1,25 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+INF = int(1e9)
 n, m = map(int, input().split())
-graph = [[n for _ in range(n)] for _ in range(n)]
-result = []
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+result = [INF]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a-1][b-1] = 1
-    graph[b-1][a-1] = 1
+    graph[a][b] = 1
+    graph[b][a] = 1
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                graph[i][j] = 0
+for i in range(n+1):
+    for j in range(n+1):
+        if i == j:
+            graph[i][j] = 0
 
-            else:
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-for i in range(n):
-    result.append(sum(graph[i]))
 
-print(result.index(min(result)) + 1)
+for i in range(1, n+1):
+    result.append(sum(graph[i][1:]))
+
+print(result.index(min(result)))
