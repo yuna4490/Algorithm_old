08e5dd622c9a000af1909d6c1cb2a7fcc4d291c3
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-03-22 13:51:05 +0900

    0325 다시!!!

--- 백트래킹/10971 외판원 순회 2.py
+++ 백트래킹/10971 외판원 순회 2.py
@@ -3,29 +3,26 @@ input = sys.stdin.readline
 
 n = int(input())
 W = [list(map(int, input().split())) for _ in range(n)]
-
-visited = [0] * n
 result = sys.maxsize
+visited = [0] * n
 
-def dfs(start, now, cnt, sum):
+def backtracking(start, now, cnt, sum):
     global result
     if cnt == n:
         if W[now][start]:
-            sum += W[now][start]
-        
-            result = min(result, sum)
+            result = min(result, sum + W[now][start])
         return
     
     for i in range(n):
         if not visited[i] and W[now][i]:
             visited[i] = 1
-            dfs(start, i, cnt+1, sum + W[now][i])
+            backtracking(start, i, cnt + 1, sum + W[now][i])
             visited[i] = 0
 
 # start 설정
 for i in range(n):
     visited[i] = 1
-    dfs(i, i, 1, 0)
+    backtracking(i, i, 1, 0)
     visited[i] = 0
 
 print(result)
