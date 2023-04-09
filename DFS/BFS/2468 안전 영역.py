f45f3079a3378e43358581e64b400f981173b0b8
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-09 23:45:44 +0900

    백준 실버1 (P)

---
+++ DFS/BFS/2468 안전 영역.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+queue = deque()
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(height, i, j):
+    queue = deque()
+    queue.append((i, j))
+    visited[i][j] = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] > height and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
+
+result = 0
+max_value = 0
+for i in range(N):
+    for j in range(N):
+        max_value = max(max_value, graph[i][j])
+
+for k in range(0, max_value + 1):
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] > k and not visited[i][j]:
+                bfs(k, i, j)
+                cnt += 1
+
+    result = max(result, cnt)
+
+print(result)
