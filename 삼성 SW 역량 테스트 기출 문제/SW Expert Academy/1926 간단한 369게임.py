f28bea6e95df06644c2d19c0ac1edeeb03376e82
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-12 23:45:27 +0900

    D2 (P) 좀 더 간결한 코드

--- 삼성 SW 역량 테스트 기출 문제/SW Expert Academy/1926 간단한 369게임.py
+++ 삼성 SW 역량 테스트 기출 문제/SW Expert Academy/1926 간단한 369게임.py
@@ -5,23 +5,13 @@ N = int(input())
 answer = []
 
 for i in range(1, N+1):
-    clap = ''
-    if '3' in str(i):
-        for _ in range(str(i).count('3')):
-            clap += '-'
     
-    if '6' in str(i):
-        for _ in range(str(i).count('6')):
-            clap += '-'
-    
-    if '9' in str(i):
-        for _ in range(str(i).count('9')):
-            clap += '-'
-    
-    if len(clap) == 0:
+    i = str(i)
+    clap = i.count('3') + i.count('6') + i.count('9')
+
+    if clap == 0:
         answer.append(i)
-    
     else:
-        answer.append(clap)
+        answer.append("-" * clap)
 
 print(*answer)
